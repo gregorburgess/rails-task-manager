6505acc77c89c4d7d2867b4cab7efcdd2766b577
Gregory Burgess <gregorburgess@gmail.com>
2018-05-12 09:39:07 +0800

    CRUD using rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,39 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+     # Task.new(name: params[:name], title: params[:title], details: params[:details], completed: params[:completed])
+    task.save
+    redirect_to task_path(task)
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    # redirect_to task_path(task_params)
+    redirect_to task_path(task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # VERB PATH, to: 'CONTROLLER#ACTION'
+
+  resources :tasks
+
+  # get "tasks/new", to: "tasks#new", as: :new
+  # post "tasks", to: "tasks#create", as: :create
+  # get "tasks", to: "tasks#index", as: :tasks
+  # patch 'task/:id', to: 'task#update'
+  # get 'task/:id/edit', to: 'task#edit', as: :edit
+  # get "tasks/:id", to: "tasks#show", as: :task
+
 end
